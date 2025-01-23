7d9c2c1c446d99438c07b20c2f87e56f037b81dc
George Rhoten <grhoten@users.noreply.github.com>
2025-01-22 16:18:06 -0800

    Inflection-64 Convert dictionary-parser to consume Wikidata (#65)

---
+++ inflection/tools/dictionary-parser/src/main/java/org/unicode/wikidata/DictionaryEntry.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2025 Unicode Incorporated and others. All rights reserved.
+ * Copyright 2023-2024 Apple Inc. All rights reserved.
+ */
+package org.unicode.wikidata;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import javax.annotation.Nonnull;
+
+/**
+ * A dictionary entry has a lemma and a set of inflections. The lemma may have additional part of speech information
+ * that should be included in the inflections. Rare inflections are sorted last so that they're accessible,
+ * but not directly referencable by default, which is important when there may be more than one inflection with the same
+ * set of grammemes.
+ */
+public class DictionaryEntry implements Comparable<DictionaryEntry> {
+
+    final String phrase;
+    private final TreeSet<String> lemmas;
+    private final TreeSet<Enum<?>> grammemes;
+    private final List<InflectionPattern> inflectionalPatterns;
+    private final List<InflectionPattern> rareInflectionalPatterns = new ArrayList<>();
+
+    public static final Comparator<InflectionPattern> INFLECTION_PATTERN_COMPARATOR = Comparator
+            .comparing(InflectionPattern::getCount)
+            .reversed();
+
+    public DictionaryEntry(String phrase, boolean isLemmaRare, TreeSet<Enum<?>> grammemes, InflectionPattern inflectionalPattern) {
+        this(phrase, phrase, isLemmaRare, grammemes, inflectionalPattern);
+    }
+
+    public DictionaryEntry(String phrase, String lemmas, boolean isLemmaRare, TreeSet<Enum<?>> grammemes, InflectionPattern inflectionalPattern) {
+        this.phrase = phrase;
+        this.lemmas = new TreeSet<>(Collections.singleton(lemmas));
+        this.grammemes = new TreeSet<>(grammemes.comparator());
+        this.grammemes.addAll(grammemes);
+        if (inflectionalPattern != null) {
+            this.inflectionalPatterns = new ArrayList<>(2);
+            this.inflectionalPatterns.add(inflectionalPattern);
+            if (isLemmaRare) {
+                rareInflectionalPatterns.add(inflectionalPattern);
+            }
+        }
+        else {
+            this.inflectionalPatterns = new ArrayList<>();
+        }
+    }
+
+    public void merge(DictionaryEntry entry) {
+        this.grammemes.addAll(entry.grammemes);
+        for (InflectionPattern inflectionalPattern : entry.inflectionalPatterns) {
+            if (!this.inflectionalPatterns.contains(inflectionalPattern)) {
+                this.inflectionalPatterns.add(inflectionalPattern);
+            }
+        }
+        for (InflectionPattern inflectionalPattern : entry.rareInflectionalPatterns) {
+            if (!this.rareInflectionalPatterns.contains(inflectionalPattern)) {
+                this.rareInflectionalPatterns.add(inflectionalPattern);
+            }
+        }
+        this.lemmas.addAll(entry.lemmas);
+    }
+
+    public Set<Enum<?>> getGrammemes() {
+        return grammemes;
+    }
+
+    @Override
+    public String toString() {
+        return this.toString(true, false);
+    }
+
+    public String toString(boolean isInflectional, boolean withLemmaForm) {
+        StringBuilder sb = new StringBuilder(256);
+        sb.append(phrase).append(':');
+        for (Enum<?> enumVal : grammemes) {
+            sb.append(' ').append(enumVal);
+        }
+        if (isInflectional) {
+            if (inflectionalPatterns.size() > 1) {
+                inflectionalPatterns.sort(INFLECTION_PATTERN_COMPARATOR);
+                if (rareInflectionalPatterns.size() > 1) {
+                    rareInflectionalPatterns.sort(INFLECTION_PATTERN_COMPARATOR);
+                }
+            }
+            for (InflectionPattern inflectionPattern : inflectionalPatterns) {
+                if (!rareInflectionalPatterns.contains(inflectionPattern)) {
+                    sb.append(" inflection=").append(inflectionPattern.getID());
+                }
+            }
+            for (InflectionPattern inflectionPattern : rareInflectionalPatterns) {
+                sb.append(" inflection=").append(inflectionPattern.getID());
+            }
+        }
+        if (withLemmaForm) {
+            for (String lemmaForm : lemmas) {
+                if (!lemmaForm.equals(this.phrase)) {
+                    sb.append(" lemma=").append(lemmaForm);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(@Nonnull DictionaryEntry o) {
+        return phrase.compareTo(o.phrase);
+    }
+}
---
+++ inflection/tools/dictionary-parser/src/main/java/org/unicode/wikidata/InflectionPattern.java
@@ -0,0 +1,350 @@
+/*
+ * Copyright 2025 Unicode Incorporated and others. All rights reserved.
+ * Copyright 2014-2024 Apple Inc. All rights reserved.
+ */
+package org.unicode.wikidata;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
+/**
+ * An inflection pattern is a non-unique pattern to represent an ordered set of inflections.
+ * For example, take the singular form of "city" and the plural form of "cities".
+ * The lemma suffix is y (singular), and making the plural form of cities changes that y to an ies suffix.
+ * @author George Rhoten 2014/11/18
+ */
+public class InflectionPattern {
+    private String id;
+    private int count = 1; // We have at least ourselves.
+    List<String> lemmaSuffixes = new ArrayList<>(1);
+    List<TreeSet<Enum<?>>> grammemeList = new ArrayList<>(1);
+    List<Inflection> inflectionSuffixes;
+    /** Are there multiple non-unique surface forms for the set of grammatical states? */
+    boolean inflectional;
+
+    public InflectionPattern(int id, String lemmaSuffix, TreeSet<Enum<?>> grammemeSet, List<Inflection> inflectionSuffixes) {
+        this(Integer.toHexString(id), lemmaSuffix, grammemeSet, inflectionSuffixes);
+    }
+
+    InflectionPattern(String id, String lemmaSuffix, TreeSet<Enum<?>> grammemeSet, List<Inflection> inflectionSuffixes) {
+        this.id = id;
+        this.grammemeList.add(grammemeSet);
+        this.lemmaSuffixes.add(lemmaSuffix);
+        this.inflectionSuffixes = inflectionSuffixes;
+        inflectional = isInflectional();
+    }
+
+    public void merge(InflectionPattern inflectionPattern) {
+        for (String suffix : inflectionPattern.lemmaSuffixes) {
+            if (!lemmaSuffixes.contains(suffix)) {
+                lemmaSuffixes.add(suffix);
+            }
+        }
+        mergeGrammemes(inflectionPattern.grammemeList);
+        inflectional = isInflectional();
+        if (inflectional) {
+            List<Inflection> newSuffixes = generateMergedInflections(inflectionPattern.inflectionSuffixes);
+            if (newSuffixes == null) {
+                throw new IllegalStateException("Disjoint inflection suffixes were attempted to be merged");
+            }
+            inflectionSuffixes = newSuffixes;
+        }
+        ++count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = Integer.toHexString(id);
+    }
+
+    private static boolean isUsefulGrammeme(Enum<?> grammeme) {
+        return !(grammeme instanceof Grammar.Alternate);
+    }
+
+    private void mergeGrammemes(List<TreeSet<Enum<?>>> newGrammemeList) {
+        // Do a deep copy
+        for (TreeSet<Enum<?>> grammemeSet : newGrammemeList) {
+            if (!grammemeList.contains(grammemeSet)) {
+                TreeSet<Enum<?>> newSet = new TreeSet<>(grammemeSet.comparator());
+                for (Enum<?> enumVal : grammemeSet) {
+                    if (isUsefulGrammeme(enumVal)) {
+                        newSet.add(enumVal);
+                    }
+                }
+                grammemeList.add(newSet);
+            }
+        }
+    }
+
+    /**
+     * We're doing this so that we can use this object as a key until we know we want to keep it around.
+     */
+    public void saveInternalReferences() {
+        lemmaSuffixes = new ArrayList<>(lemmaSuffixes);
+        List<TreeSet<Enum<?>>> oldGrammemeList = grammemeList;
+        grammemeList = new ArrayList<>(this.grammemeList.size() + 1);
+        mergeGrammemes(oldGrammemeList);
+        inflectionSuffixes = new ArrayList<>(inflectionSuffixes);
+    }
+
+
+    private static Map<Class<?>, String> createEnumToAttribute()
+    {
+        Map<Class<?>, String> result = new HashMap<>();
+        result.put(Grammar.Gender.class, "gender");
+        result.put(Grammar.Number.class, "number");
+        result.put(Grammar.Person.class, "person");
+        result.put(Grammar.PartOfSpeech.class, ""); // pos
+        result.put(Grammar.Tense.class, "tense");
+        result.put(Grammar.Mood.class, "mood");
+        result.put(Grammar.Voice.class, "voice");
+        result.put(Grammar.VerbType.class, "verb-type");
+        result.put(Grammar.Aspect.class, "aspect");
+        result.put(Grammar.Case.class, "case");
+        result.put(Grammar.Animacy.class, "animacy");
+        result.put(Grammar.Definiteness.class, "definiteness");
+        result.put(Grammar.Count.class, "count");
+        result.put(Grammar.Sound.class, ""); // sound, like vowel-start or consonant-start
+        result.put(Grammar.DerivationType.class, "derivation-type");
+        result.put(Grammar.AdjectiveType.class, "adjective-type");
+        result.put(Grammar.DeclensionClass.class, "declension");
+        result.put(Grammar.Register.class, "register");
+        result.put(Grammar.FormType.class, "form-type");
+        result.put(Grammar.Polarity.class, "polarity");
+        result.put(Grammar.ComparisonDegree.class, "comparison-degree");
+        result.put(Grammar.Transitivity.class, "transitivity");
+        result.put(Grammar.Sizeness.class, "sizeness");
+        result.put(Grammar.Usage.class, "");
+        return result;
+    }
+
+    private static final Map<Class<?>, String> ENUM_TO_ATTRIBUTE = createEnumToAttribute();
+
+    private static void toAttribute(StringBuilder sb, Enum<?> grammeme) {
+        var attribute = ENUM_TO_ATTRIBUTE.get(grammeme.getClass());
+        if (attribute == null) {
+            fail(grammeme + " (" + grammeme.getClass().getName() + ") is unknown");
+        }
+        else if (!attribute.isEmpty()) {
+            sb.append(" ").append(attribute).append("=\"").append(grammeme).append('"');
+        }
+        // else recognize, but omit the attribute
+    }
+
+    private boolean isInflectional() {
+
+        boolean hasNonIgnorableInflections = false;
+
+        // The German dict is producing patterns where all suffixes have the same grammemes, because the lr-dict has word forms with non-unique metadata,
+        // but if we do not know why the forms differ, we really cannot use the pattern...
+        boolean hasDistinctGrammemes = false;
+
+        // For patterns having single inflection, if they have a non-empty printable grammeme set then it is inflectional
+        if (inflectionSuffixes.size() == 1) {
+            int numPrintableGrammemes = 0;
+            var grammemeSet = inflectionSuffixes.get(0).getGrammemeSet();
+            for (var grammeme : grammemeSet) {
+                Class<?> declaringClass = grammeme.getDeclaringClass();
+                if (!Inflection.excludedGrammemeType(declaringClass)) {
+                    numPrintableGrammemes += 1;
+                }
+            }
+            if (numPrintableGrammemes > 0) {
+                hasDistinctGrammemes = true;
+            }
+        }
+
+        Inflection firstInflectionSuffix = inflectionSuffixes.isEmpty() ? null : inflectionSuffixes.get(0);
+        for (Inflection inflectionSuffix : inflectionSuffixes) {
+            hasNonIgnorableInflections = hasNonIgnorableInflections || !isIgnorableGrammemeSet(inflectionSuffix);
+            hasDistinctGrammemes = hasDistinctGrammemes || !inflectionSuffix.getGrammemeSet().equals(firstInflectionSuffix.getGrammemeSet());
+
+            if (hasNonIgnorableInflections && hasDistinctGrammemes) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(2000);
+        sb.append("    <pattern name=\"").append(id).append("\" words=\"").append(count).append("\">\n");
+
+        // We build up the POS data this way because sometimes certain combinations create redundancies.
+        StringBuilder posBuilder = new StringBuilder(1000);
+        TreeSet<String> posSet = new TreeSet<>();
+        for (TreeSet<Enum<?>> grammemeSet : grammemeList) {
+            ArrayList<Enum<?>> posOnly = new ArrayList<>(grammemeSet.size());
+            ArrayList<Enum<?>> grammemeOnly = new ArrayList<>(grammemeSet.size());
+            for (Enum<?> grammeme : grammemeSet) {
+                if (grammeme instanceof Grammar.PartOfSpeech) {
+                    posOnly.add(grammeme);
+                } else {
+                    grammemeOnly.add(grammeme);
+                }
+            }
+            // The grammemes apply to all pos
+            for (Enum<?> pos : posOnly) {
+                posBuilder.append("        <pos>");
+                if (!grammemeOnly.isEmpty()) {
+                    posBuilder.append("<!--");
+                    for (Enum<?> grammeme : grammemeOnly) {
+                        toAttribute(posBuilder, grammeme);
+                    }
+                    posBuilder.append(" -->");
+                }
+                posBuilder.append(pos).append("</pos>\n");
+                posSet.add(posBuilder.toString());
+                posBuilder.setLength(0);
+            }
+        }
+        for (String pos : posSet) {
+            sb.append(pos);
+        }
+
+        if (inflectional) {
+            for (String suffix : lemmaSuffixes) {
+                if (suffix.isEmpty()) {
+                    sb.append("        <suffix/>\n");
+                }
+                else {
+                    sb.append("        <suffix>").append(suffix).append("</suffix>\n");
+                }
+            }
+            sb.append("        <inflections>\n");
+            for (Inflection inflection : inflectionSuffixes) {
+                if (isIgnorableGrammemeSet(inflection)) {
+                    // We don't care about this inflection.
+                    continue;
+                }
+                sb.append("            <inflection");
+
+                Map<Class<?>, List<Enum<?>>> grammemeMap = new HashMap<>();
+                for (Enum<?> grammeme : inflection.getGrammemeSet()) {
+                    Class<?> declaringClass = grammeme.getDeclaringClass();
+                    if (Inflection.excludedGrammemeType(declaringClass)) {
+                        continue;
+                    }
+                    grammemeMap.computeIfAbsent(declaringClass, k -> new ArrayList<>());
+                    grammemeMap.get(declaringClass).add(grammeme);
+
+                    if (isUsefulGrammeme(grammeme)) {
+                        toAttribute(sb, grammeme);
+                    }
+                }
+
+                grammemeMap.values().stream().filter(v -> v.size() > 1).forEach(v -> {throw new RuntimeException(String.format("Multiple values for %s: %s. Resulting xml would NOT have been well-formed.", v.get(0).getDeclaringClass(), v));});
+
+                sb.append("><t><stem/>").append(inflection.getInflection().replace("& ", "&amp; ")).append("</t></inflection>\n");
+            }
+            sb.append("        </inflections>\n");
+        }
+        sb.append("    </pattern>\n");
+        return sb.toString();
+    }
+
+    private boolean isIgnorableGrammemeSet(Inflection inflection) {
+        return isIgnorableGrammemeSet(inflection.getGrammemeSet());
+    }
+
+    public static boolean isIgnorableGrammemeSet(TreeSet<Enum<?>> grammemeSet) {
+        return grammemeSet.contains(Grammar.PartOfSpeech.ABBREVIATION);
+    }
+
+    @Nullable
+    private static Inflection findGrammemeEquivalentInflection(@Nonnull List<Inflection> inflections, @Nonnull Inflection similarInflection) {
+        TreeSet<Enum<?>> grammemeSet = similarInflection.getGrammemeSet();
+        for (Inflection inflection : inflections) {
+            if (grammemeSet.equals(inflection.getGrammemeSet()) && !similarInflection.equals(inflection)) {
+                return inflection;
+            }
+        }
+        return null;
+    }
+
+    @Nullable
+    private List<Inflection> generateMergedInflections(@Nonnull List<Inflection> otherInflections) {
+        if (inflectionSuffixes.size() != otherInflections.size() || !inflectionSuffixes.containsAll(otherInflections)) {
+            return null;
+        }
+        if (inflectionSuffixes.equals(otherInflections)) {
+            return inflectionSuffixes;
+        }
+        // They are the same size, and we have containment of the inflections, but they aren't equal.
+        // Perhaps they're in a different order.
+        // Ensure that given all the possible grammemes of the provided inflections that there is at least 1 that is not rare.
+        ArrayList<Inflection> result = new ArrayList<>(inflectionSuffixes.size());
+        if (!inflectionSuffixes.isEmpty()) {
+            ArrayList<Inflection> rare = new ArrayList<>(inflectionSuffixes.size());
+            ArrayList<Inflection> nonRare = new ArrayList<>(inflectionSuffixes.size());
+            for (Inflection inflection : inflectionSuffixes) {
+                if (inflection.isRareUsage()) {
+                    rare.add(inflection);
+                }
+                else {
+                    nonRare.add(inflection);
+                }
+            }
+            for (Inflection otherInflection : otherInflections) {
+                if (otherInflection.isRareUsage() != rare.contains(otherInflection)) {
+                    // Uh oh. We have a disjoint situation with regards to the inflection.
+                    if (findGrammemeEquivalentInflection(nonRare, otherInflection) == null) {
+                        // Oops! Nothing is better! These are disjoint sets.
+                        return null;
+                    }
+                    if (otherInflection.isRareUsage()) {
+                        // We're making this one rare when there is another one with the same constraints that isn't rare.
+                        // This is the sometimes rare scenario. We prefer the always non-rare scenario over this one.
+                        nonRare.remove(otherInflection);
+                        rare.add(0, otherInflection);
+                    }
+                    // Else this one is already rare, and the current one is fine.
+                }
+                // Else nothing needs to change
+            }
+            result.addAll(nonRare);
+            result.addAll(rare);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof InflectionPattern)) {
+            return false;
+        }
+        InflectionPattern other = (InflectionPattern)o;
+        // non-inflectional ones are just a bag of inflections without annotation. So we collapse them to the same pattern.
+        // If they're both inflectional, then verify that the suffixes can be safely merged and that the lemmas are the same.
+        return this == o || (!inflectional && !other.inflectional && grammemeList.equals(other.grammemeList))
+            || (inflectional == other.inflectional
+                && generateMergedInflections(other.inflectionSuffixes) != null
+                && lemmaSuffixes.size() == other.lemmaSuffixes.size()
+                && lemmaSuffixes.equals(other.lemmaSuffixes)
+                && grammemeList.equals(other.grammemeList));
+    }
+
+    @Override
+    public int hashCode() {
+        return inflectionSuffixes.hashCode();
+    }
+
+    private static void fail(String msg) {
+        System.out.println(msg);
+        System.exit(1);
+    }
+}

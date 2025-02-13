f2975368e1e0f80d46449355cd30947342c44e08
Nebojsa Ciric <nciric@gmail.com>
2025-02-13 10:51:17 -0800

    Implemented wikidata load

--- data/tools/wikidata_upload.py
+++ data/tools/wikidata_upload.py
@@ -1,104 +1,68 @@
 """
 Sample script for uploading lexicon data to Wikidata using tfsl library.
+
+Follow these instructions to get tfsl:
+https://phabricator.wikimedia.org/source/tool-twofivesixlex/browse/main/
+
+git clone https://phabricator.wikimedia.org/source/tool-twofivesixlex tfsl
+
+Code expects tfsl checkout location next to inflection repository.
 """
 
+#import qnames
+#import requests
+import argparse
 import csv
-import sys
-import os
-import qnames
 import functools
-import operator
 import json
+import operator
+import os
 import readline
-import requests
+import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'twofivesixlex'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'tfsl'))
 import tfsl
 
-# Depending on upload rate and amount of data you may need to use Bot account.
-username = 'YOUR_WIKIDATA_ACCOUNT'
-password = 'YOUR_WIKIDATA_PASSWORD'
+# Easy conversion from humanly readable form to/from Wikidata codes.
+WIKI_TYPES = {
+'singular': 'Q110786',
+'plural': 'Q146786',
 
-# TODOs:
-# Skip lexemes already in wikidata
+'nominative': 'Q131105',
+'genitive': 'Q146233',
+'dative': 'Q145599',
+'accusative': 'Q146078',
+'vocative': 'Q185077',
+'locative': 'Q202142',
+'instrumental': 'Q192997',
 
-already_done = [
-	'glavni ministar',
-	'diktator',
-	'znanstveni pisac',
-	'poslovni čovjek',
-	'fotoreporter',
-	'ronilac',
-	'senator',
-	'oružar',
-	'arborist',
-	'krvnik',
-	'programer',
-	'kupac',
-	'sudac',
-	'trener za snagu i kondiciju',
-	'geolog',
-	'tehnički crtač',
-	'konstabl',
-	'branič',
-	'tehnolog',
-	'vođa sastava',
-	'youtuber',
-	'zamjenik predsjednika vlade', # composed
-	'bejzbolski trener', # composed
-	'predsjednik gradske četvrti', # composed
-	'ulema', # just one
-	'guvernante', # something off?
-	'supermodel', # just one
-	'policajac',  # double meaning too close
-	'kontrabasist',
-	'vanjski igrač', # composed?
-	'pravni savjetnik', # composed?
-	'premijer', # double meaning too close
-	'tiranin',
-	'igrač ragbija', # composed
-	'predak',
-	'psihoterapeut',
-	'endokrinolog',
-	'moderni petobojac', # composed?
-	'klarinetist',
-	'prisjednik',
-	'orkestrator',
-	
-]
+'animate': 'Q51927507',
 
-singular = 'Q110786'
-plural = 'Q146786'
+# Grammatical gender
+'feminine': 'Q1775415',
+'masculine': 'Q499327',
+'neuter': 'Q1775461',
 
-nominative = 'Q131105'
-genitive = 'Q146233'
-dative = 'Q145599'
-accusative = 'Q146078'
-vocative = 'Q185077'
-locative = 'Q202142'
-instrumental = 'Q192997'
+# To be used in "sex or gender" (P21) to indicate that the human subject is a male/female
+# or "semantic gender" (P10339) to indicate that a word refers to a male/female person.
+'female': 'Q6581072',
+'male': 'Q6581097',
 
-animate = 'Q51927507'
+'gender': 'P5185',
+'semanticgender': 'P10339',
 
-feminine = 'Q1775415'
-masculine = 'Q499327'
-neuter = 'Q1775461'
-
-female = 'Q6581072'
-male = 'Q6581097'
-
-gender = 'P5185'
-itemforthissense = 'P5137'
-semanticgender = 'P10339'
-
-Croatian = 'Q6654'
-
-noun = 'Q1084'
-
+'noun': 'Q1084',
+'proper noun': 'Q147276',
+'pronouns': 'Q36224',
+'possesive pronouns': 'Q1502460',
+'adjective': 'Q34698',
+}
+WIKI_TYPES.update(dict([reversed(i) for i in WIKI_TYPES.items()]))
 
 def hr(s): return s @ tfsl.langs.hr_
 def en(s): return s @ tfsl.langs.en_
 
+"""
 def countvalues(d):
 	if len(d) == 0: return 0
 	return functools.reduce(operator.add, map(len, d.values()))
@@ -201,7 +165,7 @@ def getform(forms, cas, numbr, gendr):
 	print('*** something is off ***')
 	print(forms)
 	exit(-1)
-	
+
 def getgloss(default):
 	return editable_input('> ', default)
 
@@ -374,3 +338,73 @@ senses, forms = readWordGraph(wikidata)
 
 done = createLexemes(wikidata, senses, forms)
 print(len(done), 'done')
+"""
+def load_wikidata(lang, wiki_file):
+    """
+    Loads lemma, language and grammatical category for easy lookup.
+
+    Args:
+        lang (str): Language to filter by.
+        wiki_data (str): Path to wikidata JSON file.
+    """
+    # Lemma-language-grammatical category set to allow for duplication of
+    # different types.
+    print("Processing wikidata, it may take a while...")
+    result = set()
+    wiki_lang = tfsl.languages.get_first_lang(lang).item
+    count = 0
+    try:
+     with open(wiki_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            # File is large, ~5GB, so we load line by line.
+            # Wikidata is an array of objects, this removes commas, newlines...
+            line = line.strip('[],\n')
+            try:
+                data = json.loads(line)
+                if data['language'] != wiki_lang or lang not in data['lemmas']:
+                    continue
+                count += 1
+                lemma = data['lemmas'][lang]['value']
+                cat = data['lexicalCategory']
+                result.add('-'.join([lemma, cat, lang]))
+            except json.JSONDecodeError:
+                print(f"Warning: Invalid JSON line: {line}")
+    except FileNotFoundError:
+        print(f"Error: File '{wiki_file}' not found.")
+    except PermissionError:
+        print(f"Error: Permission denied to read file '{wiki_file}'.")
+    print(f'Collected {count} items for {lang}.')
+    return result
+		
+
+def upload_data(username, password, lang, input_file, wiki_file, test_only, delay_ms):
+    """
+    Processes the input file and uploads to Wikidata given username and password.
+
+    Args:
+        username (str): The username for authentication.
+        password (str): The password for authentication.
+		lang (str): ISO 639 language code.
+        input_file (str): The path to the input lexeme file.
+		wiki_file (str): Path to Wikidata JSON file.
+        test_only (bool): Upload if false, print out if true.
+        delay_ms (int): Upload delay in ms, for rate limiting.
+    """
+    # Load and validate input_file
+    wikidata = load_wikidata(lang, wiki_file)
+	# Skip entities that already exist in Wikifile
+	# From the reminder build tfsl Lexemes
+	# Upload or print depending on test flag
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Upload data to Wikidata requiring authentication.')
+    parser.add_argument('username', help='Username for authentication.')
+    parser.add_argument('password', help='Password for authentication.')
+    parser.add_argument('lang', help='ISO 639 language code, e.g. en, sr, es...')
+    parser.add_argument('input_file', help='Path to the input file.')
+    parser.add_argument('wiki_file', help='Path to the wikidata JSON file.')
+    parser.add_argument('--test', action='store_true', help='Test run, prints instead of upload.')
+    parser.add_argument('--delay', type=int, default=1000, help='Milliseconds between uploads for rate limiting.')
+    args = parser.parse_args()
+
+    upload_data(args.username, args.password, args.lang, args.input_file, args.wiki_file, args.test, args.delay)

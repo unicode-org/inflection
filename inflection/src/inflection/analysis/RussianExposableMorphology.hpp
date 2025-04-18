52a411063a75ed6f8b5be6e8a426eb3353f2a743
George Rhoten <grhoten@users.noreply.github.com>
2025-04-18 11:57:14 -0700

    Inflection-37 Change test to fix the test failure due to commit conflict

--- inflection/src/inflection/analysis/RussianExposableMorphology.hpp
+++ inflection/src/inflection/analysis/RussianExposableMorphology.hpp
@@ -23,11 +23,6 @@ public:
     typedef ::inflection::dialog::DictionaryLookupInflector super;
 
 private:
-    //Animacy
-    int64_t dictionaryAnimate {  };
-    int64_t dictionaryInanimate {  };
-    int64_t dictionaryAnimacyMask {  };
-
     //POS
     int64_t dictionaryNoun {  };
     int64_t dictionaryProperNoun {  };
@@ -37,14 +32,7 @@ private:
     int64_t dictionaryNumber {  };
     int64_t dictionaryPOSMask {  };
 
-    //Other properties
-    int64_t dictionaryFamilyName {  };
-
     ::std::set<::std::u16string_view> doNotInflect { u"января", u"февраля", u"марта", u"апреля", u"мая", u"июня", u"июля", u"августа", u"сентября", u"октября", u"ноября", u"декабря", u"мин" };
-    ::std::vector<int64_t> defaultLemmaAttributes {  };
-    ::std::vector<int64_t> lemmaAttributesWithoutGender {  };
-
-    std::vector<std::tuple<std::u16string_view, int64_t, int64_t>> prefixesWithPOS {  };
 
 public:
     ::icu4cxx::UnicodeSet inflectableChars { u"[\u002d]" }; // Represents the character "-"
@@ -56,9 +44,6 @@ private:
     bool isProperNoun(int64_t phraseType) const;
     bool isInflectableByMachineLearning(int64_t phraseType) const;
     bool isInflectable(::std::u16string_view word) const;
-
-    bool preserveGender(int64_t phraseType) const;
-    ::std::optional<::inflection::dictionary::Inflector_Inflection> selectLemmaInflection(const dictionary::Inflector_InflectionPattern &inflectionPattern, int64_t inflectionGrammemes, int64_t wordGrammemes) const override;
 
 public:
     ::std::optional<::std::u16string> inflectUsingDictionary(const ::std::u16string &word, const ::std::u16string &caseString, const ::std::u16string &countString, const ::std::u16string &genderString, const ::std::u16string &animacyString, const ::std::u16string &posString) const;

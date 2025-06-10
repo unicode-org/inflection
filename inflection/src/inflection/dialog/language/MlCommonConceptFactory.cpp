9523646c4da9d5cce566b805083fc71cd5af5db5
DeonaJulary <djulary@purdue.edu>
2025-06-10 12:19:47 -0500

    Add Malayalam-specific CommonConceptFactory with lists and quantities

---
+++ inflection/src/inflection/dialog/language/MlCommonConceptFactory.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2025 Apple Inc. All rights reserved.
+ */
+#include <inflection/dialog/language/MlCommonConceptFactory.hpp>
+
+namespace inflection::dialog::language {
+
+MlCommonConceptFactory::MlCommonConceptFactory(const ::inflection::util::ULocale& language)
+    : super(language)
+{
+}
+
+MlCommonConceptFactory::~MlCommonConceptFactory()
+{
+}
+
+} // namespace inflection::dialog::language

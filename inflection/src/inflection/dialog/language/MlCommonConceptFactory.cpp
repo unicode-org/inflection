6a191b63420f47bb7d7251c81cb96cde1d2cf319
DeonaJulary <djulary@purdue.edu>
2025-06-26 15:14:30 -0500

    Update Common Concept Factory files

--- inflection/src/inflection/dialog/language/MlCommonConceptFactory.cpp
+++ inflection/src/inflection/dialog/language/MlCommonConceptFactory.cpp
@@ -1,7 +1,14 @@
 /*
- * Copyright 2025 Apple Inc. All rights reserved.
+ * Copyright 2025 Unicode Incorporated and others. All rights reserved.
  */
+
 #include <inflection/dialog/language/MlCommonConceptFactory.hpp>
+
+#include <inflection/dialog/SemanticFeatureConceptBase.hpp>
+#include <inflection/grammar/synthesis/GrammemeConstants.hpp>
+#include <inflection/npc.hpp>
+
+using inflection::grammar::synthesis::GrammemeConstants;
 
 namespace inflection::dialog::language {
 
@@ -14,4 +21,31 @@ MlCommonConceptFactory::~MlCommonConceptFactory()
 {
 }
 
+inflection::dialog::SpeakableString MlCommonConceptFactory::quantifyType(
+    const ::inflection::dialog::SpeakableString& formattedNumber,
+    const SemanticFeatureConceptBase& semanticConcept,
+    bool useDefault,
+    Plurality::Rule countType) const
+{
+    ::std::unique_ptr<::inflection::dialog::SpeakableString> speakableResult;
+    if (!useDefault) {
+        ::std::unique_ptr<SemanticFeatureConceptBase> semanticConceptClone(npc(semanticConcept.clone()));
+
+        if (Plurality::Rule::ONE == countType) {
+            semanticConceptClone->putConstraint(*npc(semanticFeatureCount), GrammemeConstants::NUMBER_SINGULAR());
+        } else {
+            semanticConceptClone->putConstraint(*npc(semanticFeatureCount), GrammemeConstants::NUMBER_PLURAL());
+        }
+
+        speakableResult.reset(semanticConceptClone->toSpeakableString());
+    }
+
+    if (speakableResult == nullptr) {
+        speakableResult.reset(semanticConcept.toSpeakableString());
+    }
+
+    return quantifiedJoin(formattedNumber, *npc(speakableResult.get()), {}, countType);
+}
+
 } // namespace inflection::dialog::language
+
--- inflection/src/inflection/dialog/language/MlCommonConceptFactory.hpp
+++ inflection/src/inflection/dialog/language/MlCommonConceptFactory.hpp
@@ -1,17 +1,29 @@
 /*
- * Copyright 2025 Apple Inc. All rights reserved.
+ * Copyright 2025 Unicode Incorporated and others. All rights reserved.
  */
 #pragma once
 
 #include <inflection/dialog/language/fwd.hpp>
 #include <inflection/dialog/CommonConceptFactoryImpl.hpp>
+#include <inflection/grammar/synthesis/fwd.hpp>
+#include <inflection/dialog/Plurality.hpp>
 
-class inflection::dialog::language::MlCommonConceptFactory
-    : public CommonConceptFactoryImpl
-{
-public:
-    typedef CommonConceptFactoryImpl super;
+namespace inflection::dialog::language {
+
+class MlCommonConceptFactory : public CommonConceptFactoryImpl {
+    using super = CommonConceptFactoryImpl;
+
 public:
     explicit MlCommonConceptFactory(const ::inflection::util::ULocale& language);
     ~MlCommonConceptFactory() override;
+
+protected:
+    ::inflection::dialog::SpeakableString quantifyType(
+        const ::inflection::dialog::SpeakableString& formattedNumber,
+        const ::inflection::dialog::SemanticFeatureConceptBase& semanticConcept,
+        bool useDefault,
+        ::inflection::dialog::Plurality::Rule countType) const override;
 };
+
+} // namespace inflection::dialog::language
+

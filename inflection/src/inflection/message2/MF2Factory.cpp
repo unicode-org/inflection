4fb5b253b6031d73fd434b6d60d553d7158cba44
Frank Yung-Fong Tang <ftang@google.com>
2025-06-27 13:38:16 -0700

    Fix memory leak in MF2Factory.cpp (issue/131) (#132)
    * Fix memory leak in MF2Factory.cpp (issue/131)
    
    Need to delete the return of toSpeakableString() and getFeatureValue()
    return from InflectableStringConcept.
    
    Fix ICU cache
    
    * Use std::unique_ptr instead of raw pointer and delete
    
    * Fix per george feedback

--- inflection/src/inflection/message2/MF2Factory.cpp
+++ inflection/src/inflection/message2/MF2Factory.cpp
@@ -98,8 +98,8 @@ FormattedPlaceholder InflectionFormatter::format(
 
     switch (toFormat.getType()) {
         case UFMT_STRING: {
-            inflection::dialog::SpeakableString input(
-                toFormat.getString(errorCode));
+            UnicodeString inputString = toFormat.getString(errorCode);
+            inflection::dialog::SpeakableString input(inputString);
             inflection::dialog::InflectableStringConcept stringConcept(
                 model, input);
             for (const auto& [key, value] : options.getOptions()) {
@@ -109,7 +109,15 @@ FormattedPlaceholder InflectionFormatter::format(
                                                 value.getString(errorCode));
                 }
             }
-            result += stringConcept.toSpeakableString()->getPrint();
+            std::unique_ptr<inflection::dialog::SpeakableString> string(
+                stringConcept.toSpeakableString());
+            if (string != nullptr) {
+                result += string->getPrint();
+            } else {
+                // stringConcept return nullptr When the constrained values is an empty set
+                // Fallback to the inputString, same as the default case in switch().
+                result += inputString;
+            }
             break;
         }
         default: {
@@ -181,7 +189,8 @@ void InflectionSelector::selectKey(
         auto value = model->getFeature(opt.at(u"select").getString(errorCode));
         UnicodeString feature;
         if (value != nullptr) {
-            auto result = stringConcept.getFeatureValue(*value);
+            std::unique_ptr<inflection::dialog::SpeakableString> result(
+                stringConcept.getFeatureValue(*value));
             if (result != nullptr) {
                 feature = result->getPrint();
             }

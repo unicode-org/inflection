96b02f68edae430b4747f299285ad723cabc10a7
FrankYFTang <FrankYFTang@users.noreply.github.com>
2025-07-08 20:32:13 +0000

    Deploying to gh-pages from @ unicode-org/inflection@02b92ec01bb804a71431b64bc6677e0f6d387863 🚀

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['constraintmap_0',['constraintMap',['../structIDDisplayValue.html#a7fdba8dd6c09940332811d4c9a0d19d2',1,'IDDisplayValue']]],
-  ['constraintmaplen_1',['constraintMapLen',['../structIDDisplayValue.html#a630e0c1eda38144d598e8fc5e48202bc',1,'IDDisplayValue']]]
+  ['constraintmaplen_1',['constraintMapLen',['../structIDDisplayValue.html#a630e0c1eda38144d598e8fc5e48202bc',1,'IDDisplayValue']]],
+  ['constraints_2',['constraints',['../classinflection_1_1dialog_1_1SemanticFeatureConceptBase.html#a783903fdc965444b3bbeee836d1cfc65',1,'inflection::dialog::SemanticFeatureConceptBase']]]
 ];

5a07a94e3239edfc603f0620b440ec5e9aae455e
Frank Yung-Fong Tang <ftang@google.com>
2025-07-07 23:57:11 -0700

    Update DisplayValue.hpp (#157)

--- inflection/src/inflection/dialog/DisplayValue.hpp
+++ inflection/src/inflection/dialog/DisplayValue.hpp
@@ -55,9 +55,9 @@ public:
      */
     bool operator!=(const DisplayValue& o) const;
     /**
-     * Generates a hash code compatible with std::hash for the o.
+     * Generates a hash code compatible with std::hash for the displayValue.
      * @param displayValue The display value object to generate the hash value.
-     * @return the hash code compatible with std::hash for the o.    
+     * @return the hash code compatible with std::hash for the displayValue.    
      */
     std::size_t operator()(const DisplayValue& displayValue) const noexcept;
 

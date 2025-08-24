9c8ba4212c97e13907d3811e784a8aefaf77737a
Bhaskar Pandey1 <bhaskarpandey787@gmail.com>
2025-08-24 11:37:56 +0000

    C API

--- inflection/src/inflection/dialog/InflectableStringConcept-c.cpp
+++ inflection/src/inflection/dialog/InflectableStringConcept-c.cpp
@@ -7,6 +7,7 @@
 #include <inflection/dialog/SemanticFeatureConcept.h>
 #include <inflection/exception/ClassCastException.hpp>
 #include <inflection/util/ULocale.hpp>
+#include <inflection/dialog/SemanticFeature.hpp>
 #include <inflection/util/TypeConversionUtils.hpp>
 #include <inflection/util/Validate.hpp>
 
@@ -39,6 +40,31 @@ iinf_create(const IDSemanticFeatureModel* model, const IDSpeakableString* value,
         try {
             inflection::util::Validate::notNull(value);
             return (IDInflectableStringConcept*)new ::inflection::dialog::InflectableStringConcept((const inflection::dialog::SemanticFeatureModel*)model,*((const inflection::dialog::SpeakableString*)value));
+        }
+        catch (const ::std::exception& e) {
+            inflection::util::TypeConversionUtils::convert(e, status);
+        }
+    }
+    return nullptr;
+}
+
+INFLECTION_CAPI IDInflectableStringConcept*
+iinf_createWithConstraints(const IDSemanticFeatureModel* model, const IDSpeakableString* value, 
+                           const ::inflection::dialog::SemanticFeature* features, const char16_t** values, int32_t SemanticFeaturesLen, UErrorCode* status) {
+    if (status != nullptr && U_SUCCESS(*status)) {
+        try {
+            inflection::util::Validate::notNull(value);
+
+            ::std::map< ::inflection::dialog::SemanticFeature, ::std::u16string> initialConstraints;
+            for (int32_t i = 0; i < SemanticFeaturesLen; ++i) {
+                initialConstraints.emplace(features[i], ::std::u16string(values[i]));
+            }
+
+            return (IDInflectableStringConcept*) new ::inflection::dialog::InflectableStringConcept(
+                (const ::inflection::dialog::SemanticFeatureModel*)model,
+                *((const ::inflection::dialog::SpeakableString*)value),
+                initialConstraints
+            );
         }
         catch (const ::std::exception& e) {
             inflection::util::TypeConversionUtils::convert(e, status);
--- inflection/src/inflection/dialog/InflectableStringConcept.h
+++ inflection/src/inflection/dialog/InflectableStringConcept.h
@@ -6,7 +6,7 @@
 #include <inflection/dialog/SpeakableString.h>
 #include <inflection/dialog/SemanticFeatureConcept.h>
 #include <inflection/dialog/SemanticFeatureModel.h>
-
+#include <inflection/dialog/SemanticFeature.hpp>
 /**
  * An object that provides a way to format a word with additional grammatical category values or semantic features of a word for a given language.
  */
@@ -34,6 +34,20 @@ INFLECTION_CAPI IDInflectableStringConcept* iinf_toInflectableStringConcept(IDSe
  *        This is set to a failure when a failure has occurred during execution.
  */
 INFLECTION_CAPI IDInflectableStringConcept* iinf_create(const IDSemanticFeatureModel* model, const IDSpeakableString* value, UErrorCode* status);
+/**
+ * Constructs a concept given a semantic feature model and a speakable string
+ *
+ * @param model - The semantic feature model required to initialize the concept.
+ * @param value - The speakable string to convert to a concept
+ * @param features - The initial semantic features to apply to the concept
+ * @param values - The initial corresponding values for the semantic features
+ * @param SemanticFeaturesLen - The number of semantic features and values provided
+ * @param status Must be a valid pointer to an error code value,
+ *        which must not indicate a failure before the function call.
+ *        This is set to a failure when a failure has occurred during execution.
+ */
+INFLECTION_CAPI IDInflectableStringConcept* iinf_createWithConstraints(const IDSemanticFeatureModel* model, const IDSpeakableString* value, 
+                                                                       const inflection::dialog::SemanticFeature* features, const char16_t** values, int32_t SemanticFeaturesLen, UErrorCode* status);
 /**
  * Destructor
  */

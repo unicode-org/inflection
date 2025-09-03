59659cd26db49fe374fe9db08fa6f010a2fb00b9
grhoten <grhoten@users.noreply.github.com>
2025-09-03 15:31:48 +0000

    Deploying to gh-pages from @ unicode-org/inflection@5d2d21ff382d8f8b63b7ae26a1c8da7ca4420d09 🚀

--- search/all_c.js
+++ search/all_c.js
@@ -1,14 +1,15 @@
 var searchData=
 [
   ['malay_0',['MALAY',['../classinflection_1_1util_1_1LocaleUtils.html#ae205e0c0e76f52ed101aa2e531fbb7d5',1,'inflection::util::LocaleUtils']]],
-  ['malaysia_1',['MALAYSIA',['../classinflection_1_1util_1_1LocaleUtils.html#a0597146dfb4a43b1b7d79b040f08b736',1,'inflection::util::LocaleUtils']]],
-  ['maxfractiondigits_2',['maxFractionDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#a65a01a3b6973498f7bbf0679cc3af671',1,'inflection::dialog::NumberConcept']]],
-  ['maxfractiondigitsaboveone_3',['maxFractionDigitsAboveOne',['../classinflection_1_1dialog_1_1NumberConcept.html#a36ebf15239ec462cae654e8366e247d0',1,'inflection::dialog::NumberConcept']]],
-  ['maxintegerdigits_4',['maxIntegerDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#a4795d7af6efad6672cd1aeba2e2d60da',1,'inflection::dialog::NumberConcept']]],
-  ['mexico_5fspanish_5',['MEXICO_SPANISH',['../classinflection_1_1util_1_1LocaleUtils.html#ab028537a1a03e98f62d40a20637994de',1,'inflection::util::LocaleUtils']]],
-  ['mf2factory_6',['MF2Factory',['../classinflection_1_1message2_1_1MF2Factory.html',1,'inflection::message2']]],
-  ['minfractiondigits_7',['minFractionDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#aed02f5dba74a6ee2c8b7df2855a4a7bd',1,'inflection::dialog::NumberConcept']]],
-  ['minintegerdigits_8',['minIntegerDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#a9d29e546b6b1b901708ee4b0a1ce6f62',1,'inflection::dialog::NumberConcept']]],
-  ['missingresourceexception_9',['missingresourceexception',['../classinflection_1_1exception_1_1MissingResourceException.html',1,'inflection::exception::MissingResourceException'],['../classinflection_1_1exception_1_1MissingResourceException.html#a86c3dfcfd261e3eacf743fc8e1d6fa5c',1,'inflection::exception::MissingResourceException::MissingResourceException()']]],
-  ['multi_20threading_10',['Multi-threading',['../index.html#autotoc_md5',1,'']]]
+  ['malayalam_1',['MALAYALAM',['../classinflection_1_1util_1_1LocaleUtils.html#aba7026fbe7a60de3b7acfa0c707dfae9',1,'inflection::util::LocaleUtils']]],
+  ['malaysia_2',['MALAYSIA',['../classinflection_1_1util_1_1LocaleUtils.html#a0597146dfb4a43b1b7d79b040f08b736',1,'inflection::util::LocaleUtils']]],
+  ['maxfractiondigits_3',['maxFractionDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#a65a01a3b6973498f7bbf0679cc3af671',1,'inflection::dialog::NumberConcept']]],
+  ['maxfractiondigitsaboveone_4',['maxFractionDigitsAboveOne',['../classinflection_1_1dialog_1_1NumberConcept.html#a36ebf15239ec462cae654e8366e247d0',1,'inflection::dialog::NumberConcept']]],
+  ['maxintegerdigits_5',['maxIntegerDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#a4795d7af6efad6672cd1aeba2e2d60da',1,'inflection::dialog::NumberConcept']]],
+  ['mexico_5fspanish_6',['MEXICO_SPANISH',['../classinflection_1_1util_1_1LocaleUtils.html#ab028537a1a03e98f62d40a20637994de',1,'inflection::util::LocaleUtils']]],
+  ['mf2factory_7',['MF2Factory',['../classinflection_1_1message2_1_1MF2Factory.html',1,'inflection::message2']]],
+  ['minfractiondigits_8',['minFractionDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#aed02f5dba74a6ee2c8b7df2855a4a7bd',1,'inflection::dialog::NumberConcept']]],
+  ['minintegerdigits_9',['minIntegerDigits',['../classinflection_1_1dialog_1_1NumberConcept.html#a9d29e546b6b1b901708ee4b0a1ce6f62',1,'inflection::dialog::NumberConcept']]],
+  ['missingresourceexception_10',['missingresourceexception',['../classinflection_1_1exception_1_1MissingResourceException.html',1,'inflection::exception::MissingResourceException'],['../classinflection_1_1exception_1_1MissingResourceException.html#a86c3dfcfd261e3eacf743fc8e1d6fa5c',1,'inflection::exception::MissingResourceException::MissingResourceException()']]],
+  ['multi_20threading_11',['Multi-threading',['../index.html#autotoc_md5',1,'']]]
 ];
